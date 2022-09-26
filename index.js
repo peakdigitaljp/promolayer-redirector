75653d87e32545a87160b2ff1308c0863c960e4a
anthonyglover <anthony@peakdigital.co.jp>
2022-09-26 16:57:07 +0900

    Fixing some fuckups

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
     const currentUrl = document.currentScript.src;
     const prefixUrl = currentUrl.indexOf('dscripts') !== -1 ? 'https://dmodules.promolayer.io' : 'https://modules.promolayer.io'
 
-    // fuck all of IE right off
-    const detectIEregexp = /Trident.*rv[ :]*(\d+\.\d+)/
-    if (detectIEregexp.test(navigator.userAgent)) return;
-
     const getUIDfromURL = (url) => {
         if(!url) return false;
         const urlArray = url.split('/')
@@ -15,17 +11,23 @@
         if(urlArray.length !== 3) return false
         if(!lastSeg.length) return false;
         if(lastSeg.indexOf('promolayer.io') !== -1) return false;
+        return lastSeg;
     }
 
     function addScript( uid ) {
         var s = document.createElement( 'script' );
         s.setAttribute('type', 'module');
         s.setAttribute( 'src', prefixUrl+'?uid='+uid );
-        document.body.appendChild( s );
+        document.head.appendChild( s );
     }
 
     const UID = getUIDfromURL(currentUrl);
-    if(UID){
+
+    // fuck all of IE right off
+    const detectIEregexp = /Trident.*rv[ :]*(\d+\.\d+)/
+    const isIE = detectIEregexp.test(navigator.userAgent)
+
+    if (UID && !isIE) {
         addScript(UID)
     }
 })();
